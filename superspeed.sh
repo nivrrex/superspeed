4e3cac120f8d7b432e3d50ef2ee8c22623009953
nivrrex <nivrrex@gmail.com>
2023-04-04 10:47:58 +0800

    Update superspeed.sh

--- superspeed.sh
+++ superspeed.sh
@@ -36,10 +36,10 @@ checkpython() {
 	        echo "正在安装 Python"
 	            if [ "${release}" == "centos" ]; then
 	            		yum update > /dev/null 2>&1
-	                    yum -y install python > /dev/null 2>&1
+	                    yum -y install pypy3 > /dev/null 2>&1
 	                else
 	                	apt-get update > /dev/null 2>&1
-	                    apt-get -y install python > /dev/null 2>&1
+	                    apt-get -y install pypy3 > /dev/null 2>&1
 	                fi
 	        
 	fi
