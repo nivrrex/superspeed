e77723cb31da790b21c093c1b2d34129b59aa0e0
nivrrex <nivrrex@gmail.com>
2023-04-04 12:00:44 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -11,12 +11,12 @@ def GetIpipInfo(para):
     f = open("ip_json.json",'r')
     ijson = json.load(f)
     jjson = ijson['location']
-    print(jjson[para.encode('utf-8')])
+    print(jjson[para])
 
 def GetGeoioInfo(para):
     ip_api = urllib.request.urlopen(r'http://ip-api.com/json')
     ijson = json.loads(ip_api.read())
-    print(ijson[para.encode('utf-8')])
+    print(ijson[para])
     
 def GetDiskInfo(para):
     temp = ExecShell("df -h -P|grep '/'|grep -v tmpfs")[0];
