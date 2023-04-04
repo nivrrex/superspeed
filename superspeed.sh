61f7c5a2ab74ed7362aee1dd0c02bc2d9e0d4a6d
nivrrex <nivrrex@gmail.com>
2023-04-04 11:12:18 +0800

    Update superspeed.sh

--- superspeed.sh
+++ superspeed.sh
@@ -15,18 +15,18 @@ about() {
 	echo " \                   v2.0.0 (6 Sep 2020)                 / "
 	echo " \                   代码由 Oldking 编写                 / "
 	echo " \              修改以及汉化由 qd201211 完成             / "
+	echo " \              *二次修改*以及汉化由 nivrrex 完成             / "
 	echo " ========================================================= "
 	echo ""
-	echo " 作者文章: https://www.oldking.net/350.html"
 	echo " Copyright (C) 2019 Oldking oooldking@gmail.com"
 	echo ""
 }
 
 preinfo() {
 	echo "                   Superbench 服务器性能测试                          "
-	echo "       bash <(curl -Lso- https://git.io/superbench)"
+	echo "       bash <(curl -Lso- https://raw.githubusercontent.com/ernisn/superspeed/master/superspeed.sh)"
 	echo "       全部节点列表:  https://git.io/superspeedList"
-	echo "       节点更新: 2020/04/09  | 脚本更新: 2020/09/06"
+	echo "       节点更新: 2020/04/09  | 脚本更新: 2023/04/04"
 	echo "----------------------------------------------------------------------"
 }
 
@@ -109,41 +109,14 @@ benchinit() {
 	# check speedtest
 	if  [ ! -e './speedtest-cli/speedtest' ]; then
 		echo "正在安装 Speedtest-cli"
-	#	mkdir -p /root/speedtest-cli && wget --no-check-certificate -O /root/speedtest-cli/speedtest https://raw.githubusercontent.com/user1121114685/speedtest_cli/master/spd_cli/x86_64/speedtest  > /dev/null 2>&1
-	#fi
-	#	chmod a+rx ./speedtest-cli/speedtest
-		wget --no-check-certificate -qO speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-$(uname -m)-linux.tgz
-		#wget --no-check-certificate -qO speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-$(uname -m)-linux.tgz 
+        version=`curl -Is https://github.com/librespeed/speedtest-cli/releases/latest | grep location: | cut -d "/" -f 8 | tr '\r' ' '| cut -d " " -f 1`
+        wget --no-check-certificate -qO speedtest.tar.gz https://github.com/librespeed/speedtest-cli/releases/download/${version}/librespeed-cli_${version:1}_linux_amd64.tar.gz
 	fi
-	mkdir -p speedtest-cli && tar zxvf speedtest.tgz -C ./speedtest-cli/ > /dev/null 2>&1 && chmod a+rx ./speedtest-cli/speedtest
+	mkdir -p speedtest-cli && tar zxvf speedtest.tar.gz -C ./speedtest-cli/ > /dev/null 2>&1 && chmod a+rx ./speedtest-cli/speedtest
 
-	# install virt-what
-	#if  [ ! -e '/usr/sbin/virt-what' ]; then
-	#	echo "Installing Virt-what ..."
-	#    if [ "${release}" == "centos" ]; then
-	#    	yum update > /dev/null 2>&1
-	#        yum -y install virt-what > /dev/null 2>&1
-	#    else
-	#    	apt-get update > /dev/null 2>&1
-	#        apt-get -y install virt-what > /dev/null 2>&1
-	#    fi      
-	#fi
-
-	# install jq
-	#if  [ ! -e '/usr/bin/jq' ]; then
-	# 	echo " Installing Jq ..."
-    #		if [ "${release}" == "centos" ]; then
-	#	    yum update > /dev/null 2>&1
-	#	    yum -y install jq > /dev/null 2>&1
-	#	else
-	#	    apt-get update > /dev/null 2>&1
-	#	    apt-get -y install jq > /dev/null 2>&1
-	#	fi      
-	#fi
-	# install tools.py
 	if  [ ! -e 'tools.py' ]; then
 		echo "正在安装 tools.py ..."
-		wget --no-check-certificate https://raw.githubusercontent.com/oooldking/script/master/tools.py > /dev/null 2>&1
+		wget --no-check-certificate https://raw.githubusercontent.com/nivrrex/superspeed/master/tools.py > /dev/null 2>&1
 	fi
 	chmod a+rx tools.py
 
@@ -255,19 +228,19 @@ runtest() {
 		# speed_test '31985' '重庆' '联通'
 		 speed_test '4863' '陕西西安' '联通'
 		#***
-		# speed_test '30154' '上海' '移动'
+		 speed_test '30154' '上海' '移动'
 		# speed_test '25637' '上海５Ｇ' '移动'
 		# speed_test '26728' '云南昆明' '移动'
 		# speed_test '27019' '内蒙古呼和浩特' '移动'
-		 speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
+		# speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
 		# speed_test '30293' '内蒙古通辽５Ｇ' '移动'
 		 speed_test '25858' '北京' '移动'
-		 speed_test '16375' '吉林长春' '移动'
+		# speed_test '16375' '吉林长春' '移动'
 		# speed_test '24337' '四川成都' '移动'
 		 speed_test '17184' '天津５Ｇ' '移动'
 		# speed_test '26940' '宁夏银川' '移动'
 		# speed_test '31815' '宁夏银川' '移动'
-		# speed_test '26404' '安徽合肥５Ｇ' '移动'
+		 speed_test '26404' '安徽合肥５Ｇ' '移动'
 		 speed_test '27151' '山东临沂５Ｇ' '移动'
 		# speed_test '25881' '山东济南５Ｇ' '移动'
 		# speed_test '27100' '山东青岛５Ｇ' '移动'
@@ -287,7 +260,7 @@ runtest() {
 		# speed_test '17320' '江苏镇江５Ｇ' '移动'
 		 speed_test '25883' '江西南昌５Ｇ' '移动'
 		# speed_test '17223' '河北石家庄' '移动'
-		# speed_test '26331' '河南郑州５Ｇ' '移动'
+		 speed_test '26331' '河南郑州５Ｇ' '移动'
 		# speed_test '6715' '浙江宁波５Ｇ' '移动'
 		# speed_test '4647' '浙江杭州' '移动'
 		# speed_test '16503' '海南海口' '移动'
@@ -295,7 +268,7 @@ runtest() {
 		# speed_test '16145' '甘肃兰州' '移动'
 		 speed_test '16171' '福建福州' '移动'
 		# speed_test '18444' '西藏拉萨' '移动'
-		 speed_test '16398' '贵州贵阳' '移动'
+		# speed_test '16398' '贵州贵阳' '移动'
 		 speed_test '25728' '辽宁大连' '移动'
 		# speed_test '16167' '辽宁沈阳' '移动'
 		# speed_test '17584' '重庆' '移动'
